b0049afe8b734bd80519dd6e146a790be778a144
Poh Nean <pohnean@gmail.com>
2025-05-29 14:08:57 +0800

    gql injection

--- init.lua
+++ init.lua
@@ -1052,7 +1052,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'graphql' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -1082,7 +1082,7 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
---
+++ queries/javascript/injections.scm
@@ -0,0 +1,4 @@
+(call_expression
+  function: (identifier) @gql_tag (#eq? @gql_tag "gql")
+  arguments: (template_string) @graphql
+)
